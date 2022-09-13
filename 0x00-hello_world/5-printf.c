a98606af9c67b6ee955be27145fab8f81357edb7
keenlighte <keenlight806@gmaile.com>
2022-09-13 08:51:22 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #inlude <stdio.h>
 /**
- * main - entey point
- * Return: Always 0 (success)
+  main - entry point
+  Return: Always 0 (success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-

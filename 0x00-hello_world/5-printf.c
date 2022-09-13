c200204365ca6dbfc65d18a303bc801ae31b932a
keenlighte <keenlight806@gmaile.com>
2022-09-13 07:52:15 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#inlude <stdio.h>
 /**
- * main - entry point
- * Return: Always 0 (success)
+ * main - entey point
+ * Retrun: Always 0 (success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
 

3bc1545c33d35bdf860465efed41643a5bd287d7
keenlighte <keenlight806@gmaile.com>
2022-09-15 15:01:10 -0700

    i 0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *  * main - Entry mode
+ *   * Return: Always 0
+ *    */
+int main(void)
+{
+		int i;
+
+			i = 0;
+				positive_or_negative(i);
+
+					return (0);
+}

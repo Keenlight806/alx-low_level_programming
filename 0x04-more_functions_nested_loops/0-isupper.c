bc27b793471064baaeada2de9fc5e2e418315384
keenlighte <keenlight806@gmaile.com>
2022-09-15 14:09:44 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,14 @@
-#include "main.h" 
-#include <ctype.h>
-/**
- * _isuper - Entry mode
- * @c: an integer
- * Return: Always 0
- */
-int _isupper(int c)
-{
-	if (isupper(c))
-		return (1);
-	else
-		return (0);
-}
+nclude "main.h" 
+ #include <ctype.h> 
+ /** 
+  *   * _isupper - Entry mode 
+  *    * @c: an integer 
+  *     * Return: Always 0 
+  *      */ 
+ int _isupper(int c) 
+	 { 
+		          if (isupper(c)) 
+			                   return (1); 
+		           else 
+			                    return (0); 
+		    }

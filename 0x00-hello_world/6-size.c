e19fa7360319cdfe5e9d4cdb3b72cc842da879fd
keenlighte <keenlight806@gmaile.com>
2022-09-17 22:41:08 -0700

    hgf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
-/*
- * main - Entry point
- * Descrition: Print the size of various types on the computer
- * Return: returns 0
- **/
+/**
+ *  * main - Entry point
+ *   * Return: Always 0 (Success)
+ *    */
 int main(void)
 {
-	        printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
-	        printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
-	        printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
-	        printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
-	        printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
-	return (0);
+		char c;
+			int i;
+				long int li;
+					long long ll;
+						float f;
+
+							printf("Size of a char: %ld byte(s)\n", (unsigned int) sizeof(c));
+								printf("Size of an int: %ld byte(s)\n", (unsigned int) sizeof(i));
+									printf("Size of a long int: %ld byte(s)\n", (unsigned long) sizeof(li));
+										printf("Size of a long long int: %ld byte(s)\n", (unsigned long) sizeof(ll));
+											printf("Size of a float: %ld byte(s)\n", (unsigned int) sizeof(f));
+												return (0);
 }

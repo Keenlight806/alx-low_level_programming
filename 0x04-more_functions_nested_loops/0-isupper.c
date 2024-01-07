361d5a65d63c3f680eb936e83286a58423287276
keenlight806 <keenlight806@gmail.com>
2024-01-07 06:25:51 -0800

    my folder

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,21 @@
-#include "main.h" 
- #include <ctype.h> 
- /** 
-  *   * _isupper - Entry mode 
-  *    * @c: an integer 
-  *     * Return: Always 0 
-  *      */ 
- int _isupper(int c) 
-	 { 
-		          if (isupper(c)) 
-			                   return (1); 
-		           else 
-			                    return (0);
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isupper - checks for uppercase letters
+ *
+ * @c: parametr to be printed
+ * Return: 0 or 1 depending on the condition
+ */
+
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
